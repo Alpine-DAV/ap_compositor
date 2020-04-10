a257deff0459a394c84f06f5ad666afc61c7011b
mclarsen <larsen30@llnl.gov>
2020-04-10 09:21:54 -0700

    test intel 17 compiler

---
+++ host_configs/toss3_intel_17.cmake
@@ -0,0 +1,19 @@
+#######
+# using intel 17
+#######
+
+# c compiler
+set("CMAKE_C_COMPILER" "/usr/tce/packages/intel/intel-17.0.2/bin/icc" CACHE PATH "")
+
+# cpp compiler
+set("CMAKE_CXX_COMPILER" "/usr/tce/packages/intel/intel-17.0.2/bin/icpc" CACHE PATH "")
+
+# OPENMP Support
+set("ENABLE_OPENMP" "ON" CACHE PATH "")
+
+# MPI Support
+set("ENABLE_MPI" "ON" CACHE PATH "")
+
+set("MPI_C_COMPILER" "/usr/tce/packages/mvapich2/mvapich2-2.3-intel-17.0.2/bin/mpicc" CACHE PATH "")
+
+set("MPI_CXX_COMPILER" "/usr/tce/packages/mvapich2/mvapich2-2.3-intel-17.0.2/bin/mpic++" CACHE PATH "")
